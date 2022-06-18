34a0e54e43d0fcb01e6b13816f57b28278bba984
Hayden Singleton <hgs@vt.edu>
2022-06-18 09:41:48 -0400

    Style change from linter

--- demo/src/components/GameBoard.vue
+++ demo/src/components/GameBoard.vue
@@ -2,13 +2,13 @@
   <div class="content">
     <div class="columns" style="margin-top: 25px">
       <div class="column" @click="score = ++score % 6">
-        <p class="button" id="score">{{ score }}/5</p>
+        <p id="score" class="button">{{ score }}/5</p>
       </div>
       <div class="level">
         <LaneItem
-          class="level-item"
           v-for="item in items"
           :key="item.id"
+          class="level-item"
         ></LaneItem>
       </div>
       <div class="column">Info</div>
@@ -21,9 +21,9 @@ import LaneItem from "@/components/LaneItem.vue";
 
 export default {
   name: "GameBoard",
+  components: { LaneItem },
   data() {
     return { score: 0, items: [1, 2, 3, 4] };
   },
-  components: { LaneItem },
 };
 </script>
