ecc767ccd7dd656c102ac556ec8fea756976fdfc
Hayden Singleton <hgs@vt.edu>
2022-06-21 15:53:31 -0400

    Added video embed and made hero larger

--- demo/src/components/TutorialMenu.vue
+++ demo/src/components/TutorialMenu.vue
@@ -1,12 +1,29 @@
 <template>
-  <section class="hero is-info is-small">
+  <section class="section hero is-info is-small">
     <div class="hero-head">
       <p class="title">How to Play</p>
     </div>
+    <div class="hero-body">
+      <section class="section">
+        <div>
+          <iframe
+            width="720"
+            height="480"
+            src="https://www.youtube.com/embed/jj1QJLUC_M0"
+            title="How to Play Inscryption"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </section>
+    </div>
     <div class="hero-foot">
-      <p class="subtitle">Learn the basics of our game</p>
+      <p class="subtitle">Learn the basics of our game from ExtraOrdiNora</p>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<style>
+iframe {
+  border: none;
+}
+</style>
