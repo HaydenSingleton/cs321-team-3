fcac92c83f8b090fbfcabc856de7cc443cc9794d
Hayden Singleton <hgs@vt.edu>
2022-07-03 14:03:28 -0400

    Added some tests, not full coverage yet

--- tests/Tile.spec.ts
+++ tests/Tile.spec.ts
@@ -1,44 +1,48 @@
-// //import { mount } from "@vue/test-utils";
-// import { Tile } from "@/tile.ts";
-//
-// describe("Tile Javascript", () => {
-//   it("defaultValuesOfTile", () => {
-//     var nt = new Tile();
-//     expect(nt.isEmpty).toBe(true);
-//     expect(nt.attack).toBe(0);
-//     expect(nt.health).toBe(0);
-//     expect(nt.sigil).toBe("empty");
-//   }),
-//     it("resetTile", () => {
-//       var nt = new Tile(false, 1, 3, "deathtouch");
-//       nt.reset();
-//       expect(nt.isEmpty).toBe(true);
-//       expect(nt.attack).toBe(0);
-//       expect(nt.health).toBe(0);
-//       expect(nt.sigil).toBe("empty");
-//     }),
-//     it("AssignTile", () => {
-//       var nt = new Tile();
-//       nt.Assign(false, 1, 3, "deathtouch");
-//       expect(nt.isEmpty).toBe(false);
-//       expect(nt.attack).toBe(1);
-//       expect(nt.health).toBe(3);
-//       expect(nt.sigil).toBe("deathtouch");
-//     }),
-//     it("killTileButSurvives", () => {
-//       var nt = new Tile(false, 1, 10, "deathtouch");
-//       nt.death();
-//       expect(nt.isEmpty).toBe(false);
-//       expect(nt.attack).toBe(1);
-//       expect(nt.health).toBe(10);
-//       expect(nt.sigil).toBe("deathtouch");
-//     }),
-//     it("killTile", () => {
-//       var nt = new Tile(false, 1, 0, "deathtouch");
-//       nt.death();
-//       expect(nt.isEmpty).toBe(true);
-//       expect(nt.attack).toBe(0);
-//       expect(nt.health).toBe(0);
-//       expect(nt.sigil).toBe("empty");
-//     });
-// });
+//import { mount } from "@vue/test-utils";
+import Tile from "@/tile";
+
+describe("Tile class tests", () => {
+  it("defaultValuesOfTile", () => {
+    const nt = new Tile();
+    expect(nt.isEmpty).toBe(true);
+    expect(nt.attack).toBe(0);
+    expect(nt.health).toBe(0);
+    expect(nt.sigill).toBe("empty");
+    expect(nt.toString()).toBe("[id=0 ()]");
+  }),
+    it("fromValues", () => {
+      const nt = Tile.fromValues(1, 3, "empty");
+      expect(nt.isEmpty).toBe(false);
+      expect(nt.attack).toBe(1);
+      expect(nt.health).toBe(3);
+      expect(nt.sigill).toBe("empty");
+      expect(nt.toString()).toBe("[id=1 (1 3 empty)]");
+    }),
+    it("fromList", () => {
+      const nt = Tile.fromList([3, 1, "tri_strike"]);
+      expect(nt.isEmpty).toBe(false);
+      expect(nt.attack).toBe(3);
+      expect(nt.health).toBe(1);
+      expect(nt.sigill).toBe("tri_strike");
+    }),
+    it("testIDs", () => {
+      const nt = Tile.fromValues(1, 10, "deathtouch");
+      expect(nt.isEmpty).toBe(false);
+      expect(nt.attack).toBe(1);
+      expect(nt.health).toBe(10);
+      expect(nt.sigill).toBe("deathtouch");
+      expect(nt.id).toBe(3);
+    }),
+    it("reset", () => {
+      const nt = Tile.fromValues(1, 0, "deathtouch");
+      expect(nt.isEmpty).toBe(false);
+      expect(nt.attack).toBe(1);
+      expect(nt.health).toBe(0);
+      expect(nt.sigill).toBe("deathtouch");
+      nt.reset();
+      expect(nt.isEmpty).toBe(true);
+      expect(nt.attack).toBe(0);
+      expect(nt.health).toBe(0);
+      expect(nt.sigill).toBe("empty");
+    });
+});
